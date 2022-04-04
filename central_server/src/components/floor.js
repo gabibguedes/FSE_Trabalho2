c8b11058924366cff02b8e1a9d2a1b33008fe9d7
Gabriela Guedes <gabrielabguedes@gmail.com>
2022-04-04 19:37:57 -0300

    Style dashboard
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- central_server/src/components/floor.js
+++ central_server/src/components/floor.js
@@ -39,21 +39,23 @@ const floor = (name, data) => {
   }
 
   return `
-    <div style="display: flex; flex-direction: column; border: 1px solid ${dark_blue}; border-radius: 5px; padding: 20px; margin:20px">
-      <div style="display: flex;">
-        <div style="display=flex;">
-          <h2 style="color: ${dark_blue}">${name}</h2>
-          <p>Temperatura: <b>${data.temperature}ºC</b></p>
-          <p>Humidade: <b>${data.humidity}%</b></p>
-          <p>Pessoas: <b>${data.people_count}</b></p>
-        </div>
-        <div style="display: flex; flex-direction: column; padding: 20px">
-          ${render_buttons()}
-        </div>
+    <div style="display: flex; justify-content: space-between; border: 1px solid ${dark_blue}; border-radius: 5px; padding: 20px; margin:20px">
+      <div style="display=flex;">
+        <h2 style="color: ${dark_blue}">${name}</h2>
+        <p>Temperatura: <b>${data.temperature}ºC</b></p>
+        <p>Humidade: <b>${data.humidity}%</b></p>
+        <p>Pessoas: <b>${data.people_count}</b></p>
       </div>
-      <div>
+      <div style="padding: 10px">
+        <p><b>Sensores</b></p>
         ${render_sensor_list(data.inputs)}
+      </div>
+      <div style="padding: 10px">
+        <p><b>Saídas</b></p>
         ${render_sensor_list(data.outputs)}
+      </div>
+      <div style="display: flex; flex-direction: column; padding: 20px">
+        ${render_buttons()}
       </div>
     </div>
     <script>
--- central_server/src/pages/dashboard.js
+++ central_server/src/pages/dashboard.js
@@ -3,18 +3,29 @@ const floor = require('../components/floor')
 const { get_socket_data } = require('../socket')
 
 const dashboard = () => {
+  const data = get_socket_data()
+  let people_sum = 0
 
-  let data = get_socket_data()
   const render_floors = () => {
     const keys = Object.keys(data)
     return keys.map(key => (
       floor(key, data[key])
     )).join(' ')
   }
+
+  const total_people_count = () => {
+    Object.keys(data).forEach(key => {
+      people_sum += data[key].people_count
+    })
+  }
+
+  total_people_count()
+
   return html(`
     <div style="padding: 20px 50px 0 50px">
       <h1>Dashboard do Servidor Central</h1>
-      <div style="display: flex; justify-content: space-evenly">
+      <p>Total de pessoas no prédio: <b>${people_sum}</b></p>
+      <div>
         ${render_floors()}
       </div>
     </div>
